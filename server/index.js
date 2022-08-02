617c5b8aa05802e24c00fc1f037131722dcc7b6b
John <johnudoumoh63@gmail.com>
2022-08-02 11:30:22 +0100

    security updates

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
@@ -14,9 +16,6 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://johndavid:lifevision@cluster0.mzyv4.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(process.env.PORT, () => console.log(`server running on port: ${process.env.PORT}`)))
 .catch(error => console.log(error.message))
