1a4356c38b1456ae72f00fb135746703649455eb
John <johnudoumoh63@gmail.com>
2022-08-15 13:09:18 +0100

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import {Container} from '@material-ui/core'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import PostDetails from './components/PostDetails/PostDetails.js'
 import Navbar from './components/navbar/Navbar.js'
 import Home from './components/Home/Home'
 import Auth from './components/Auth/Auth.js'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'))
 
     return (
         <GoogleOAuthProvider clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}>
         <BrowserRouter>
-       <Container maxwidth='lg'>
+       <Container maxwidth='xl'>    
             <Navbar />
             <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/auth" exact component={Auth} />
+                <Route path="/" exact component={() => <Redirect to='/posts'/>}/>
+                <Route path='/posts' exact component={Home}/>
+                <Route path='/posts/search' exact component={Home}/>
+                <Route path='/posts/:id' component={PostDetails}/>
+                <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to='/posts'/>)} />
             </Switch>
         </Container>
         </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,28 @@
 import React, {useState, useEffect} from 'react'
-import {Grow, Grid, Container, Paper} from '@material-ui/core'
+import {Grow, Grid, Container, Paper, AppBar, TextField, Button} from '@material-ui/core'
 import { useDispatch } from 'react-redux'
+import { useHistory, useLocation } from 'react-router-dom'   
+import ChipInput from 'material-ui-chip-input'
 
 import Posts from '../Posts/posts'
 import Form from '../Form/form'
 import { getPosts } from '../../actions/posts'
 import Paginate from '../Paginate/Paginate'
+import useStyles from './styles'
+
+function useQuery(){
+    return new URLSearchParams(useLocation().search)
+}
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(null)
     const dispatch = useDispatch()
+    const classes = useStyles()
+    const query = useQuery()
+    const history = useHistory()
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         dispatch(getPosts())
@@ -17,12 +30,15 @@ const Home = () => {
 
   return (
       <Grow in>
-          <Container>
-              <Grid  container justifyContent='space-between' alignItems='stretch' spacing={3}>
-                  <Grid item xs={12} sm={7}>
+          <Container maxWidth="xl">
+              <Grid  container justifyContent='space-between' alignItems='stretch' spacing={3} className={classes.gridContainer}>
+                  <Grid item xs={12} sm={6} md={8}>
                       <Posts setCurrentId={setCurrentId} />
                   </Grid>
-                  <Grid item xs={12} sm={4}>
+                  <Grid item xs={12} sm={6} md={4}>
+                    <AppBar className={classes.appBarSearch} position='static' color='inherit'>
+                        <TextField name='search' variant='outlined' label='Search Memories' fullWidth value={search} onChange={(e) => {setSearch(e.target.value)}}/>
+                    </AppBar>
                       <Form currentId={currentId} setCurrentId={setCurrentId} />
                       <Paper elevation={6}>
                         <Paginate/>
